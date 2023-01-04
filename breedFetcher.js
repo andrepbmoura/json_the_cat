5499353ea66c5396a621ab89f7c6dc733d8b31d9
Andre <andrepaesbm@gmail.com>
2023-01-04 12:02:44 -0700

    created a test using mocha/chai and a index.js file

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,20 @@
 const request = require('request');
 
-const fetchBreed = function(breed, callback) {
+const fetchBreedDescription = function(breed, callback) {
   const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
   request.get(url, (error, response) => {
     const body = JSON.parse(response.body)[0];
-    let status;
+    let desc;
     if (body === undefined) {
       error = "Breed not found: " + breed;
     } else {
-      status = (response.statusCode !== 200) ? `Status Code: ${response.statusCode}` : body["description"];
+      desc = (response.statusCode !== 200) ? `Status Code: ${response.statusCode}` : body["description"];
     }
     callback(error, desc);
   });
 };
 
-module.exports = { fetchBreed };
+module.exports = { fetchBreedDescription };
+
+
+
